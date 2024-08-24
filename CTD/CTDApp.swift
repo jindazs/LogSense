04392eec50ac7c2b8448f66c7a2486cd68972572
jindazs <nodoyuki@gmail.com>
2024-08-25 07:36:29 +0900

    Initial Commit

---
+++ CTD/CTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CTDApp.swift
+//  CTD
+//
+//  Created by Yuki Jin on 2024/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CTD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CTD
+//
+//  Created by Yuki Jin on 2024/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
