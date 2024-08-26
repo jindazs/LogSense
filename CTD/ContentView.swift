f41bb3d738ba6efc366c03c7dd29d0425ff387e7
jindazs <nodoyuki@gmail.com>
2024-08-26 22:01:47 +0900

    add today date botton

--- CTD/ContentView.swift
+++ CTD/ContentView.swift
@@ -8,15 +8,15 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             TabView {
-                WebView(url: URL(string: "https://scrapbox.io/\(projectName)")!)
+                WebViewWrapper(url: URL(string: "https://scrapbox.io/\(projectName)")!)
                     .tabItem {
                         Text("メイン")
                     }
-                WebView(url: URL(string: "https://scrapbox.io/\(projectName)/ToDo")!)
+                WebViewWrapper(url: URL(string: "https://scrapbox.io/\(projectName)/ToDo")!)
                     .tabItem {
                         Text("ToDo")
                     }
-                WebView(url: URL(string: "https://scrapbox.io/\(projectName)/\(getCurrentDate())")!)
+                WebViewWrapper(url: URL(string: "https://scrapbox.io/\(projectName)/\(getCurrentDate())")!)
                     .tabItem {
                         Text("日付")
                     }
@@ -68,13 +68,44 @@ struct ContentView: View {
         dateFormatter.dateFormat = "yyyy-MM-dd"
         return dateFormatter.string(from: Date())
     }
+    
+    func getCurrentDateForInput() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M月d日"
+        return "#\(dateFormatter.string(from: Date()))"
+    }
 }
 
-struct WebView: UIViewRepresentable {
+class CustomWebView: WKWebView {
+    override var inputAccessoryView: UIView? {
+        let accessoryView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 44))
+        accessoryView.backgroundColor = UIColor.systemGray5
+        
+        let button = UIButton(type: .system)
+        button.setTitle("今日の日付を挿入", for: .normal)
+        button.frame = CGRect(x: 10, y: 5, width: 150, height: 34)
+        button.addTarget(self, action: #selector(insertDate), for: .touchUpInside)
+        
+        accessoryView.addSubview(button)
+        
+        return accessoryView
+    }
+    
+    @objc func insertDate() {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "M月d日"
+        let dateString = "#\(dateFormatter.string(from: Date()))"
+        
+        let script = "document.execCommand('insertText', false, '\(dateString)');"
+        self.evaluateJavaScript(script, completionHandler: nil)
+    }
+}
+
+struct WebViewWrapper: UIViewRepresentable {
     let url: URL
     
     func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
+        let webView = CustomWebView()
         
         // カスタムユーザーエージェントを設定
         webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1"
